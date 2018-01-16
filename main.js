68b06cf7d1e138279566ba2e59a5f2f2d7276a2c
west2407 <34055446+westwood2407@users.noreply.github.com>
2018-01-16 19:43:37 +1100

    Assignment04
    final01

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log('connected');
+$('.readmore a').click(function() {
+  event.preventDefault();
+  $('#show-this-on-click').slideDown()
+  $('.readmore').hide()
+  $('.readless').show()
+}
+)
+
+
+$('.readless a').click(function() {
+  event.preventDefault();
+  $('#show-this-on-click').slideUp()
+  $('.readless').hide()
+  $('.readmore').show()
+}
+)
+
+$('.learnmore').click(function() {
+  event.preventDefault();
+  $('#learnmoretext').slideDown()
+  $('.learnmore').hide()
+
+}
+)
